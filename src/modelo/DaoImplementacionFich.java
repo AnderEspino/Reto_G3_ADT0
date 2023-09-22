edf1c86b27d1665391300e9526050d1aae099538
Adrian <adrianmosan@gmail.com>
2023-09-22 09:32:47 +0200

    Implementacion ficheros en curso.

--- src/modelo/DaoImplementacionFich.java
+++ src/modelo/DaoImplementacionFich.java
@@ -5,10 +5,73 @@
  */
 package modelo;
 
+import clases.ConvocatoriaExamen;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author 2dam
  */
-public class DaoImplementacionFich {
-    
+public class DaoImplementacionFich implements DAO {
+   File fich = new File("CExamen.obj"); 
+   
+   private static void volcarListFich(List<ConvocatoriaExamen> convocatoria, File fich) {
+		// TODO Auto-generated method stub
+		ObjectOutputStream oos = null;
+
+		try {
+			oos = new ObjectOutputStream(new FileOutputStream(fich));
+
+			for (int i = 0; i < convocatoria.size(); i++) {
+				oos.writeObject(convocatoria.get(i));
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				oos.flush();
+				oos.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+   
+   private static List<ConvocatoriaExamen> volcarFichList(File fich) {
+		// TODO Auto-generated method stub
+		List<ConvocatoriaExamen> convocatoria = new ArrayList<>();
+
+		// flujo para leer
+		ObjectInputStream ois = null;
+		ConvocatoriaExamen cExamen;
+
+		try {
+			ois = new ObjectInputStream(new FileInputStream(fich));
+
+			while (true) {
+				cExamen = (ConvocatoriaExamen) ois.readObject();
+				convocatoria.add(cExamen);
+			}
+		} catch (EOFException e) {
+			System.out.println("Final de fichero");
+		} catch (IOException e) {
+			System.out.println("Error de entrada salida");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error al castear el obj");
+			e.printStackTrace();
+		}
+
+		return convocatoria;
+	}
 }
